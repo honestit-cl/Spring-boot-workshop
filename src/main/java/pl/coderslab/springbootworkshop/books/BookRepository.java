e5d3bc4eacf0192eef755cb0eae99de815268829
honestit <michal.kupisinski@gmail.com>
2022-09-18 18:15:57 +0200

    Repository for book entity

---
+++ src/main/java/pl/coderslab/springbootworkshop/books/BookRepository.java
@@ -0,0 +1,6 @@
+package pl.coderslab.springbootworkshop.books;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
